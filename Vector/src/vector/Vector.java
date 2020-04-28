f3f0c15176439502c60a5e1697736ff4430456c8
Angela Samsonova <angela.v.samsonova@gmail.com>
2020-04-29 01:49:24 +0700

    Vector fixed.

--- Vector/src/vector/Vector.java
+++ Vector/src/vector/Vector.java
@@ -3,44 +3,57 @@ package vector;
 import java.util.Arrays;
 
 public class Vector {
-    private int n;
     private double[] elements;
 
-    public Vector(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("n must be > 0");
+    public Vector(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("size must be > 0");
         }
 
-        this.n = n;
-        this.elements = new double[n];
+        elements = new double[size];
     }
 
     public Vector(Vector vector) {
-        this(vector.n, vector.elements);
+        this(vector.elements);
     }
 
     public Vector(double[] array) {
         this(array.length, array);
     }
 
-    public Vector(int n, double[] array) {
-        if (n <= 0) {
-            throw new IllegalArgumentException("n must be > 0");
+    public Vector(int size, double[] array) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("size must be > 0");
         }
 
-        this.n = n;
-        this.elements = new double[n];
+        elements = new double[size];
 
-        System.arraycopy(array, 0, elements, 0, Math.min(array.length, n));
+        System.arraycopy(array, 0, elements, 0, Math.min(array.length, size));
+    }
+
+    public double getElement(int index) {
+        return elements[index];
+    }
+
+    public void setElement(int index, double newValue) {
+        elements[index] = newValue;
     }
 
     public int getSize() {
-        return n;
+        return elements.length;
     }
 
     @Override
     public String toString() {
-        return Arrays.toString(this.elements);
+        StringBuilder sb = new StringBuilder("{");
+
+        for (double element : elements) {
+            sb.append(element).append(", ");
+        }
+
+        sb.replace(sb.length() - 2, sb.length(), "}");
+
+        return sb.toString();
     }
 
     @Override
@@ -54,33 +67,57 @@ public class Vector {
         }
 
         Vector vector = (Vector) o;
-        return n == vector.n &&
-                Arrays.equals(elements, vector.elements);
+        return Arrays.equals(elements, vector.elements);
     }
 
     @Override
     public int hashCode() {
         final int prime = 37;
         int hash = 1;
-        hash = prime * hash + Integer.hashCode(n);
         hash = prime * hash + Arrays.hashCode(elements);
         return hash;
     }
 
-    //
-    public void addVector(Vector vector) {
-        Vector newVector = addVectors(this, vector);
+    public void add(Vector vector) {
+        if (vector == null) {
+            throw new IllegalArgumentException("vector must be not null");
+        }
 
-        this.n = newVector.n;
-        this.elements = newVector.elements;
+        if (getSize() > vector.getSize()) {
+            double[] extendedArray = Arrays.copyOf(vector.elements, getSize());
+            vector = new Vector(extendedArray);
+
+            for (int i = 0; i < getSize(); i++) {
+                elements[i] += vector.elements[i];
+            }
+        } else {
+            elements = Arrays.copyOf(elements, vector.getSize());
+
+            for (int i = 0; i < vector.getSize(); i++) {
+                elements[i] += vector.elements[i];
+            }
+        }
     }
 
-    //
-    public void subtractVector(Vector vector) {
-        Vector newVector = subtractVectors(this, vector);
+    public void subtract(Vector vector) {
+        if (vector == null) {
+            throw new IllegalArgumentException("vector must be not null");
+        }
 
-        this.n = newVector.n;
-        this.elements = newVector.elements;
+        if (getSize() > vector.getSize()) {
+            double[] extendedArray = Arrays.copyOf(vector.elements, getSize());
+            vector = new Vector(extendedArray);
+
+            for (int i = 0; i < getSize(); i++) {
+                elements[i] -= vector.elements[i];
+            }
+        } else {
+            elements = Arrays.copyOf(elements, vector.getSize());
+
+            for (int i = 0; i < vector.getSize(); i++) {
+                elements[i] -= vector.elements[i];
+            }
+        }
     }
 
     public void multiplyByScalar(double x) {
@@ -89,108 +126,67 @@ public class Vector {
         }
     }
 
-    public void revertVector() {
-        this.multiplyByScalar(-1);
+    public void revert() {
+        multiplyByScalar(-1);
     }
 
     public double getLength() {
-        double sumOfElements = 0;
+        double elementsSquaresSum = 0; //поменять имя по смыслу
 
         for (double e : elements) {
-            sumOfElements += Math.pow(e, 2.0);
+            elementsSquaresSum += Math.pow(e, 2.0);
         }
 
-        return Math.sqrt(sumOfElements);
+        return Math.sqrt(elementsSquaresSum);
     }
 
-    public void getAndChangeElement(int index, double newValue) {
-        elements[index] = newValue;
-    }
-
-    public static Vector addVectors(Vector vector1, Vector vector2) {
+    public static Vector add(Vector vector1, Vector vector2) {
         if (vector1 == null || vector2 == null) {
             throw new IllegalArgumentException("vector must be not null");
         }
 
-        Vector resultingVector;
-        int resultingVectorSize;
+        Vector newvector = new Vector(vector1);
+        newvector.add(vector2);
 
-        if (vector1.getSize() > vector2.getSize()) {
-            resultingVectorSize = vector1.getSize();
-            resultingVector = new Vector(resultingVectorSize);
-
-            double[] extendedArray = Arrays.copyOf(vector2.elements, resultingVectorSize);
-            Vector newVectorFromVector2 = new Vector(resultingVectorSize, extendedArray);
-
-            for (int i = 0; i < resultingVectorSize; i++) {
-                resultingVector.elements[i] = vector1.elements[i] + newVectorFromVector2.elements[i];
-            }
-        } else {
-            resultingVectorSize = vector2.getSize();
-            resultingVector = new Vector(resultingVectorSize);
-
-            double[] extendedArray = Arrays.copyOf(vector1.elements, resultingVectorSize);
-            Vector newVectorFromVector1 = new Vector(resultingVectorSize, extendedArray);
-
-            for (int i = 0; i < resultingVectorSize; i++) {
-                resultingVector.elements[i] = vector2.elements[i] + newVectorFromVector1.elements[i];
-            }
-        }
-
-        return resultingVector;
+        return newvector;
     }
 
-    public static Vector subtractVectors(Vector vector1, Vector vector2) {
+    public static Vector subtract(Vector vector1, Vector vector2) {
         if (vector1 == null || vector2 == null) {
             throw new IllegalArgumentException("vector must be not null");
         }
 
-        vector2.revertVector();
+        Vector newvector = new Vector(vector1);
+        newvector.subtract(vector2);
 
-        Vector newVector = addVectors(vector1, vector2);
-
-        vector2.revertVector();
-
-        return newVector;
+        return newvector;
     }
 
-    public static Vector multiplyVectors(Vector vector1, Vector vector2) {
+    public static double getScalarProduct(Vector vector1, Vector vector2) {
         if (vector1 == null || vector2 == null) {
             throw new IllegalArgumentException("vector must be not null");
         }
 
-        Vector resultingVector;
-        int resultingVectorSize;
+        double sum = 0;
 
         if (vector1.getSize() == vector2.getSize()) {
-            resultingVectorSize = vector1.getSize();
-            resultingVector = new Vector(resultingVectorSize);
-
-            for (int i = 0; i < resultingVectorSize; i++) {
-                resultingVector.elements[i] = vector1.elements[i] * vector2.elements[i];
+            for (int i = 0; i < vector1.getSize(); i++) {
+                sum += vector1.elements[i] * vector2.elements[i];
             }
         } else if (vector1.getSize() > vector2.getSize()) {
-            resultingVectorSize = vector1.getSize();
-            resultingVector = new Vector(resultingVectorSize);
+            double[] extendedArray = Arrays.copyOf(vector2.elements, vector1.getSize());
 
-            double[] extendedArray = Arrays.copyOf(vector2.elements, resultingVectorSize);
-            Vector newVectorFromVector2 = new Vector(resultingVectorSize, extendedArray);
-
-            for (int i = 0; i < resultingVectorSize; i++) {
-                resultingVector.elements[i] = vector1.elements[i] * newVectorFromVector2.elements[i];
+            for (int i = 0; i < vector1.getSize(); i++) {
+                sum += vector1.elements[i] * extendedArray[i];
             }
         } else {
-            resultingVectorSize = vector2.getSize();
-            resultingVector = new Vector(resultingVectorSize);
+            double[] extendedArray = Arrays.copyOf(vector1.elements, vector2.getSize());
 
-            double[] extendedArray = Arrays.copyOf(vector1.elements, resultingVectorSize);
-            Vector newVectorFromVector1 = new Vector(resultingVectorSize, extendedArray);
-
-            for (int i = 0; i < resultingVectorSize; i++) {
-                resultingVector.elements[i] = vector2.elements[i] * newVectorFromVector1.elements[i];
+            for (int i = 0; i < vector2.getSize(); i++) {
+                sum += vector2.elements[i] * extendedArray[i];
             }
         }
 
-        return resultingVector;
+        return sum;
     }
 }
--- Vector/src/vector_main/VectorDemo.java
+++ Vector/src/vector_main/VectorDemo.java
@@ -15,28 +15,29 @@ public class VectorDemo {
         Vector vector3 = new Vector(vector2);
         Vector vector4 = new Vector(4, array1);
         Vector vector5 = new Vector(2, array1);
-        Vector vector6 = addVectors(vector3, vector5);
-        Vector vector7 = addVectors(vector5, vector5);
+
+        Vector vector6 = add(vector3, vector5);
+        Vector vector7 = add(vector5, vector5);
         Vector vector8 = new Vector(array4);
         vector8.multiplyByScalar(2);
         Vector vector9 = new Vector(array4);
-        vector9.revertVector();
-        Vector vector10 = subtractVectors(vector5, vector3);
+        vector9.revert();
+        Vector vector10 = subtract(vector5, vector3);
         Vector vector11 = new Vector(array4);
-        vector11.subtractVector(vector2);
+        vector11.subtract(vector2);
         Vector vector12 = new Vector(array4);
-        vector12.addVector(vector2);
+        vector12.add(vector2);
         Vector vector13 = new Vector(array1);
         vector13.getLength();
         Vector vector14 = new Vector(array1);
-        vector14.getAndChangeElement(2, 99.0);
-        Vector vector15 = multiplyVectors(vector2,vector5);
+        vector14.setElement(2, 99.0);
 
         System.out.println("vector1 = " + vector1.toString());
         System.out.println("vector2 = " + vector2.toString());
         System.out.println("vector3 = " + vector3.toString());
         System.out.println("vector4 = " + vector4.toString());
         System.out.println("vector5 = " + vector5.toString());
+        System.out.println("element of vector2 by index 1  = " + vector2.getElement(1));
         System.out.println("vector2 equals vector3 = " + vector2.equals(vector3));
         System.out.println("vector3 equals vector4 = " + vector3.equals(vector4));
         System.out.println("vector6 = " + vector6.toString());
@@ -48,6 +49,10 @@ public class VectorDemo {
         System.out.println("vector12 = " + vector12.toString());
         System.out.println("vector13 length = " + vector13.getLength());
         System.out.println("vector14 = " + vector14.toString());
-        System.out.println("vector15 = " + vector15.toString());
+        System.out.println("scalar product of vector2 and vector5 = " + getScalarProduct(vector2, vector5));
+
+        Vector vector15 = new Vector(3);
+        vector15.setElement(1, 3);
+        System.out.println("3.0 set as element of vector15 by index 1 = " + vector15);
     }
 }
